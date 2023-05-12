68e35d42d3817eb14ef15894012ac0b098d327c3
Luanda Vital <luavital0@gmail.com>
2023-05-11 22:19:10 -0300

    projeto 2: inclusão de fonte, loading, status bar, header, ícones e componente de título

--- 2-ignite-teams/App.tsx
+++ 2-ignite-teams/App.tsx
@@ -1,11 +1,22 @@
+import { StatusBar } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { ThemeProvider } from 'styled-components';
+
+import { Loading } from '@components/Loading';
 import theme from './src/theme';
 import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups></Groups>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Groups></Groups> : <Loading />}
     </ThemeProvider>
   );
 }
